b16dcf61f94c09f6f9d4f5833f8384be63a875a4
Chirayu Desai <chirayudesai1@gmail.com>
2019-09-19 20:16:48 +0530

    Non-working Android.bp
    * Missing kotlinx, and maybe others.
    * Need to add commons-io and novacrypto libs

--- Android.mk
+++ Android.mk
@@ -15,19 +15,3 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/sysconfig
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-
-backup_root := $(LOCAL_PATH)
-
-$(backup_root)/Backup.apk:
-	cd $(backup_root) && ./download.sh
-
-LOCAL_MODULE := Backup
-LOCAL_SRC_FILES := Backup.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_CLASS := APPS
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_DEX_PREOPT := false
-LOCAL_REQUIRED_MODULES := permissions_com.stevesoltys.backup.xml whitelist_com.stevesoltys.backup.xml
-include $(BUILD_PREBUILT)
